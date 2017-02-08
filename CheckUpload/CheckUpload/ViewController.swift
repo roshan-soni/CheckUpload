3a668f7901c5b19b1bfbcadcbcd64d8c1c057546
roshan-soni <roshan.soni@appirio.com>
2017-02-08 15:57:39 +0530

    Add files via upload

---
+++ CheckUpload/CheckUpload/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckUpload
+//
+//  Created by Roshan Soni on 08/02/17.
+//  Copyright © 2017 Roshan Soni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
